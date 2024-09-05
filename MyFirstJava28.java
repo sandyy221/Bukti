950fb04bedaae775538ce877c4824371df927a02
sandyy221 <kurniawansandy808@gmail.com>
2024-09-05 10:00:53 +0700

    Add files via upload

---
+++ MyFirstJava28.java
@@ -0,0 +1,8 @@
+public class MyFirstJava28 {
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Sandy Kurniawan");
+        //System.out.print("\nSaya adalah Mahasiswa baru jurusan Teknologi Infromasi-POLINEMA");
+
+    
+    }
+}
